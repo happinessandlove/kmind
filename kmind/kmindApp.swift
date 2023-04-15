435db319950d217c2d1553a9ae5000ce00f50c38
jeremy <1565803020@qq.com>
2023-04-15 17:16:19 +0800

    Initial Commit

---
+++ kmind/kmindApp.swift
@@ -0,0 +1,17 @@
+//
+//  kmindApp.swift
+//  kmind
+//
+//  Created by jeremy on 2023/4/15.
+//
+
+import SwiftUI
+
+@main
+struct kmindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
