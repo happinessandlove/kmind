1d986497701987402a7d38c79a82395f09c78270
jeremy <1565803020@qq.com>
2023-04-15 23:14:23 +0800

    init

--- kmind/ContentView.swift
+++ kmind/ContentView.swift
@@ -1,21 +1,53 @@
-//
-//  ContentView.swift
-//  kmind
-//
-//  Created by jeremy on 2023/4/15.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    @State private var username: String = ""
+    @State private var password: String = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            VStack {
+                Text("Kmind")
+                    .font(Device.current == .mac ? .system(size: 48) : .largeTitle)
+                    .fontWeight(.bold)
+                    .padding(.bottom, Device.current == .mac ? 80 : 50)
+                
+                VStack(alignment: .leading) {
+                    Text("用户")
+                        .font(.headline)
+                    TextField("Enter your username", text: $username)
+                        .padding()
+                        .background(Color(.white))
+                        .cornerRadius(8.0)
+                        .padding(.bottom, 20)
+                    
+                    Text("密码")
+                        .font(.headline)
+                    SecureField("Enter your password", text: $password)
+                        .padding()
+                        .background(Color(.white))
+                        .cornerRadius(8.0)
+                        .padding(.bottom, 20)
+                }
+                .padding(.horizontal, Device.current == .mac ? 80 : 40)
+                
+                Button(action: {
+                    print("Login button tapped")
+                }) {
+                    Text("Login")
+                        .font(.headline)
+                        .frame(minWidth: 0, maxWidth: .infinity)
+                        .padding()
+                        .background(Color.blue)
+                        .cornerRadius(8.0)
+                        .foregroundColor(.white)
+                }
+                .padding(.horizontal, Device.current == .mac ? 80 : 40)
+                
+                Spacer()
+            }
+            .padding(.top, Device.current == .mac ? 150 : 100)
         }
-        .padding()
     }
 }
 
